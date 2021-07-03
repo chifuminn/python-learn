2089b4b31d22f8629f8edf70e002833637f96d59
黒川智史 <kurokawachifumi@kurokawasatoshinoMacBook-Air.local>
2021-07-03 11:34:10 +0900

    7/3の勉強

---
+++ python.py
@@ -0,0 +1,18 @@
+print('')
+print('hello' + 'world')
+print('')
+
+print(11 + 11)
+print('11' + '11')
+
+
+maiyann = '白石'
+
+# ここからif文
+maiyann_old = 12
+
+if maiyann_old < 28:
+    print('後輩です')
+else:
+    print('白石本人です')    
+
